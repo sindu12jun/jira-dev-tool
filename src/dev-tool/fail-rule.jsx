730822a42d5d7e609112aedd8c82a9901267dc61
JUN LI <imleelijun@gmail.com>
2020-12-27 14:23:21 +1100

    fix fail rule

--- src/dev-tool/fail-rule.jsx
+++ src/dev-tool/fail-rule.jsx
@@ -1,40 +1,42 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button, Form, Input, List, Select, Typography } from 'antd'
 import { useLocalStorageState } from '../util'
+import { useForm } from 'antd/lib/form/Form'
 
-const { Text, Link } = Typography;
+const { Text, Link } = Typography
 
-const { Option } = Select;
+const { Option } = Select
 
 export const FailRule = () => {
-  const [method, setMethod] = useState("all");
-  const [url, setUrl] = useState("");
-
+  const [form] = useForm()
   const [failConfig, setFailConfig] = useLocalStorageState(
-    "__jira_request_fail_config__",
+    '__jira_request_fail_config__',
     []
-  );
+  )
 
   function handleRemoveClick(index) {
-    setFailConfig((c) => [...c.slice(0, index), ...c.slice(index + 1)]);
+    setFailConfig((c) => [...c.slice(0, index), ...c.slice(index + 1)])
   }
 
-  function handleSubmit() {
-    if (!method && !url) {
-      alert("请指定请求方法或URL");
-      return;
+  function handleSubmit(values) {
+    const { method, url } = values
+    if (!url) {
+      alert('请指定URL')
+      return
     }
     setFailConfig((c) => [
       ...c,
-      { requestMethod: method.toUpperCase(), urlMatch: url },
-    ]);
-    setMethod("");
-    setUrl("");
+      { requestMethod: method.toUpperCase(), urlMatch: url }
+    ])
+    form.setFieldsValue({
+      method: 'all',
+      url: ''
+    })
   }
 
   const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select value={method} onChange={setMethod} style={{ width: 100 }}>
+    <Form.Item name="method" noStyle>
+      <Select style={{ width: 100 }}>
         <Option value="all">ALL</Option>
         <Option value="get">GET</Option>
         <Option value="post">POST</Option>
@@ -42,48 +44,45 @@ export const FailRule = () => {
         <Option value="patch">PATCH</Option>
       </Select>
     </Form.Item>
-  );
+  )
 
   return (
     <div
       style={{
-        display: "grid",
-        gridColumnGap: "30px",
-        gridTemplateColumns: "1.2fr 2fr",
+        display: 'grid',
+        gridColumnGap: '30px',
+        gridTemplateColumns: '1.2fr 2fr'
       }}
     >
-      <Form onFinish={handleSubmit} style={{ paddingTop: "12px" }}>
+      <Form form={form} initialValues={{ method: 'all' }} onFinish={handleSubmit} style={{ paddingTop: '12px' }}>
         <Form.Item
           name="url"
-          rules={[{ required: true, message: "请输入url" }]}
         >
           <Input
-            value={url}
-            onChange={(event) => setUrl(event.target.value)}
             addonBefore={prefixSelector}
-            placeholder={"/projects"}
+            placeholder={'/projects'}
           />
         </Form.Item>
         <Form.Item>
-          <Button htmlType={"submit"}>添加</Button>
+          <Button htmlType={'submit'}>添加</Button>
         </Form.Item>
       </Form>
       <List
-        header={"列表"}
+        header={'列表'}
         dataSource={failConfig}
         renderItem={(item, index) => (
           <List.Item
             actions={[
               <a onClick={() => handleRemoveClick(index)} key="delete">
                 删除
-              </a>,
+              </a>
             ]}
           >
-            <Text mark> 请求方法：{item.requestMethod || "无"} </Text>
-            <Typography.Text>URL: {item.urlMatch || "无"}</Typography.Text>
+            <Text mark> 请求方法：{item.requestMethod || '无'} </Text>
+            <Typography.Text>URL: {item.urlMatch || '无'}</Typography.Text>
           </List.Item>
         )}
       />
     </div>
-  );
-};
+  )
+}
