cf9ef15c75628f544ca6585167af0d3311155662
JUN LI <imleelijun@gmail.com>
2020-12-28 22:49:16 +1100

    toArray

--- src/server/handlers/rest-handlers.js
+++ src/server/handlers/rest-handlers.js
@@ -1,54 +1,57 @@
 import { rest } from 'msw'
 import { getUser } from './account'
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL
+
+const tryToArray = (value) => Array.isArray(value) ? value.map(Number) : Number(value)
 
 const convertIds = (object) => {
-  const result = {};
+  const result = {}
   Object.keys(object).forEach((key) => {
     // 如果包含Id，比如personId，就要转换成数字
-    result[key] = key.includes("Id") ? Number(object[key]) : object[key];
-  });
-  return result;
-};
+    result[key] = key.includes('Id') ? tryToArray(object[key]) : object[key]
+  })
+  object.id = tryToArray(object.id)
+  return result
+}
 
 export const getRestHandlers = (endpoint, db) => {
   return [
     // query list
     rest.get(`${apiUrl}/${endpoint}`, async (req, res, ctx) => {
-      const user = await getUser(req);
-      const params = req.url.searchParams;
+      const user = await getUser(req)
+      const params = req.url.searchParams
       const queryResult = db.queryByOwnerId(
         user.id,
         Object.fromEntries(params)
-      );
-      return res(ctx.json(queryResult));
+      )
+      return res(ctx.json(queryResult))
     }),
     // query detail
     rest.get(`${apiUrl}/${endpoint}/:id`, async (req, res, ctx) => {
-      const { id } = req.params;
-      const item = db.detail(id);
-      return res(ctx.json(item));
+      const { id } = req.params
+      const item = db.detail(id)
+      return res(ctx.json(item))
     }),
     // put item
     rest.patch(`${apiUrl}/${endpoint}/:id`, async (req, res, ctx) => {
-      const { id } = convertIds(req.params);
-      const updates = req.body;
-      const updatedItem = db.update(id, updates);
-      return res(ctx.json(updatedItem));
+      const { id } = convertIds(req.params)
+      const updates = req.body
+      const updatedItem = db.update(id, updates)
+      return res(ctx.json(updatedItem))
     }),
 
     // remove item
     rest.delete(`${apiUrl}/${endpoint}/:id`, async (req, res, ctx) => {
-      const { id } = req.params;
-      db.remove(id);
-      return res(ctx.json({ success: true }));
+      const { id } = req.params
+      db.remove(id)
+      return res(ctx.json({ success: true }))
     }),
 
     // create item
     rest.post(`${apiUrl}/${endpoint}`, async (req, res, ctx) => {
-      const user = await getUser(req);
-      const targetAddItem = Object.assign(req.body, { ownerId: user.id });
+      const user = await getUser(req)
+      const targetAddItem = Object.assign(req.body, { ownerId: user.id })
 
       // const nameExist = !!db
       //   .queryByOwnerId(user.id)
@@ -59,8 +62,8 @@ export const getRestHandlers = (endpoint, db) => {
       //   throw error;
       // }
 
-      const detail = await db.create(convertIds(targetAddItem));
-      return res(ctx.json(detail));
-    }),
-  ];
-};
+      const detail = await db.create(convertIds(targetAddItem))
+      return res(ctx.json(detail))
+    })
+  ]
+}
